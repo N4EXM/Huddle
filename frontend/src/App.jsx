a97f36181622e4c43752c1d5d1e573050db7b8e9
N4EXM <naemmmail89@gmail.com>
2025-09-04 15:50:01 +0100

    completed the sidebar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LoadingPage from './pages/LoadingPage'
+import DashboardPage from './pages/DashboardPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom' 
 
 function App() {
 
+  const [loading, setIsloading] = useState(false)
+
   return (
-    <div
-      className='text-text'
-    >
-      <LoadingPage></LoadingPage>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route
+            index
+            path='/'
+            element={<DashboardPage/>}
+          />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
+
 
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <div
+      className='w-full h-screen bg-background p-8 grid grid-cols-12 grid-rows-12 text-text gap-4'
+    >
+      <Sidebar/>
+    </div>
   )
 }
 
